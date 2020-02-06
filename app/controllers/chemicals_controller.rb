2f5195bb94c6bafa266cde373c8cf8bb8323461c
wataru.ishida <cheero.iw@gmail.com>
2020-02-06 11:48:37 +0000

    add columns

--- app/controllers/chemicals_controller.rb
+++ app/controllers/chemicals_controller.rb
@@ -1,6 +1,8 @@
 class ChemicalsController < ApplicationController
   def index
     @chemicals = Chemical.all
+    # @chemicals = Chemical.find_by(how_to_use: "殺虫剤")
+    # @chemicals = Chemical.where(how_to_use: params[:sort])
   end 
   
   def show 
